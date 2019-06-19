8704cf1eca296033c1e4436ceaa4359f0e9ae299
Admin <Admin@DESKTOP-UQPODTH>
2019-06-19 18:56:45 +0530

    Forms folder structure has been changed to support Gulp Task - moved project under code folder.

---
+++ src/Feature/Forms/code/Actions/SubmitAIEAction.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using Newtonsoft.Json;
+using RestSharp;
+using Sitecore.Links;
+using Sitecore.Configuration;
+using Sitecore.ExperienceForms.Processing.Actions.Models;
+using AIEnterprise.Feature.Forms.DataDictionary;
+using AIEnterprise.Feature.Forms.Helper;
+using AIEnterprise.Feature.Forms.Models;
+//using ServiceMaster.SVMBrands.Feature.SVMForms.Models;
+//using ServiceMaster.SVMBrands.Feature.SVMForms.Helper;
+//using ServiceMaster.SVMBrands.Foundation.Common;
+//using Sitecore.Data.Items;
+//using ServiceMaster.SVMBrands.Foundation.WildCards.Models;
+
+namespace AIEnterprise.Feature.Forms.Actions
+{
+    public class SubmitAIEAction : SubmitActionBase<RedirectActionData>
+    {
+        Dictionary<string,
+        List<string>> hstable = new Dictionary<string,
+        List<string>>();
+
+
+        string toEmailID = Sitecore.Context.Site.SiteInfo.Properties.Get("AIEToEmail");
+        string subjectEmail = Sitecore.Context.Site.SiteInfo.Properties.Get("AIEEmailSubject");
+        string isOAFMailEnabled = Sitecore.Context.Site.SiteInfo.Properties.Get("AIEEmailEnabled");
+        
+
+        public SubmitAIEAction(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+
+        }
+
+        protected override bool TryParse(string value, out RedirectActionData target)
+        {
+            target = new RedirectActionData { ReferenceId = new Guid() };
+            return true;
+        }
+
+        string currentPage = "/";
+        public override void ExecuteAction(FormSubmitContext formSubmitContext, string parameters)
+        {
+            try
+            {
+                bool excutethis = this.Execute("", formSubmitContext);
+                var thankyou = currentPage + "?status=success";
+                Sitecore.Diagnostics.Log.Info("thank you page url" + thankyou, "");
+                var defaultUrlOptions = LinkManager.GetDefaultUrlOptions();
+                defaultUrlOptions.SiteResolving = Settings.Rendering.SiteResolving;
+                formSubmitContext.RedirectUrl = thankyou;
+                formSubmitContext.RedirectOnSuccess = true;
+                formSubmitContext.Abort();
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error in Form Submission" + e.Message, "");
+                Log.Error("Error in Form Submission2" + e.StackTrace, "");
+                Log.Error("Error in Form Submission1" + e.InnerException.ToString(), "");
+            }
+        }
+
+
+
+
+        public bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(data, nameof(data));
+            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+
+            //Get all Data Posted from FORM
+            var formDict = new FormDictionary();
+            var hstable = formDict.GetFieldsDictionary(formSubmitContext.Fields);
+            bool isMailSent = SendEmailNotification(hstable);
+            return true;
+        }
+
+        private bool SendEmailNotification(Dictionary<string, List<string>> formdata)
+        {
+            try
+            {
+                //Send the form details via emails
+                EmailHelper emailHeler = new EmailHelper();
+                string emailFrom = emailHeler.GetValuefromDictionary(formdata, "your-email");
+                string subject = subjectEmail;
+                var allEmails = toEmailID.Split(';');
+                var isMailSent = false;
+                var emailBody = emailHeler.GetEmailBody(formdata);
+
+                foreach (var email in allEmails)
+                {
+                    var aieEmail = new Email
+                    {
+                        To = email,
+                        Subject = subject,
+                        From = emailFrom,
+                        Body = emailBody
+                    };
+
+                    isMailSent = emailHeler.Send(aieEmail);
+                    if (!isMailSent)
+                    {
+                        Log.Error("$Email sender failed", "FormSubmission");
+                    }
+                }
+                return isMailSent;
+
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error in Form sending Form Data via mail : " + e.StackTrace + e.Message, "");
+                return false;
+            }
+        }
+
+        private string GetFieldById(object firstNameFieldId, IList<IViewModel> fields)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool Execute(RedirectActionData data, FormSubmitContext formSubmitContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Feature/Forms/code/Helper/EmailHelper.cs
@@ -0,0 +1,130 @@
+﻿using Sitecore.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using AIEnterprise.Feature.Forms.Models;
+
+namespace AIEnterprise.Feature.Forms.Helper
+{
+    public class EmailHelper
+    {
+        public string GetEmailBody(Dictionary<string, List<string>> formData)
+        {
+            string emailHtml = string.Empty;
+            try
+            {
+                var htmlEmailTemplate = GetEmailTemplate("AIE_ContactEmail");
+
+                if (htmlEmailTemplate == null)
+                {
+                    Sitecore.Diagnostics.Log.Info("Email Template is null", "");
+                    return null;
+                }
+                
+                
+                htmlEmailTemplate = htmlEmailTemplate.Replace("#Name#", GetValuefromDictionary(formData, "your-name"));
+                htmlEmailTemplate = htmlEmailTemplate.Replace("#Email#", GetValuefromDictionary(formData, "your-email"));
+                htmlEmailTemplate = htmlEmailTemplate.Replace("#Message#", GetValuefromDictionary(formData, "your-message"));
+                
+                //emailHtml = htmlEmailTemplate.ReplacePatternCaseInsensitive(replacements);
+                emailHtml = htmlEmailTemplate;
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Info($"Email failed to send: ", ex.Message);
+            }
+            return emailHtml;
+        }
+
+        public string RootPath => HttpContext.Current.Request.PhysicalApplicationPath;
+
+        public string GetEmailTemplate(string template)
+        {
+            try
+            {
+                var path = string.Format("{0}EmailTemplate\\{1}.html", RootPath, template);
+                var html = File.ReadAllText(path);
+                return html;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Send(Email email)
+        {
+            var recipients = GetRecipients(email);
+            var sitecoreEmail = new MailMessage(email.From, recipients.First(), email.Subject, email.Body);
+            sitecoreEmail.IsBodyHtml = true;
+            try
+            {
+                using (var client = CreateSmtpClient())
+                {
+                    Sitecore.Diagnostics.Log.Info("recipients.First()" + recipients.First(), "");
+                    Sitecore.Diagnostics.Log.Info("email.Subject" + email.Subject, "");
+                    Sitecore.Diagnostics.Log.Info("Email From address" + email.From, "");
+                    if (client == null)
+                        Sitecore.Diagnostics.Log.Info("SMTP Client is NULL", "");
+                    client.Send(sitecoreEmail);
+                }
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("SmtpClient not created:" + email.From + "--" + ex.StackTrace, "");
+                return false;
+            }
+            return true;
+        }
+
+        private string[] GetRecipients(Email email)
+        {
+            return email.To.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        }
+
+
+        private static SmtpClient CreateSmtpClient()
+        {
+            string mailServer = Settings.MailServer;
+            Sitecore.Diagnostics.Log.Info("SmtpClient ServerName:" + mailServer, "");
+            SmtpClient smtpClient;
+            if (string.IsNullOrEmpty(mailServer))
+            {
+                smtpClient = new SmtpClient();
+            }
+            else
+            {
+                int mailServerPort = Settings.MailServerPort;
+                smtpClient = mailServerPort <= 0 ? new SmtpClient(mailServer) : new SmtpClient(mailServer, mailServerPort);
+            }
+            string mailServerUserName = Settings.MailServerUserName;
+            bool enableSsl;
+            smtpClient.EnableSsl = bool.TryParse(Sitecore.Configuration.Settings.GetSetting("Mail.MailServerEnableSsl"), out enableSsl) &&
+                enableSsl;
+            if (mailServerUserName.Length > 0)
+            {
+                string mailServerPassword = Settings.MailServerPassword;
+                NetworkCredential networkCredential = new NetworkCredential(mailServerUserName, mailServerPassword);
+                smtpClient.Credentials = networkCredential;
+            }
+            return smtpClient;
+        }
+
+        public string GetValuefromDictionary(Dictionary<string, List<string>> formData, string FieldName)
+        {
+            string fieldValue = string.Empty;
+            if (formData.ContainsKey(FieldName))
+            {
+                List<string> fieldValues = new List<string>();
+                if (formData.TryGetValue(FieldName, out fieldValues))
+                    fieldValue = fieldValues[0];
+            }
+            return fieldValue;
+        }
+
+    }
+}
