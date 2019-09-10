ec78b9477f712820450a9a8cee6a30418590f2f0
Sudheer Kumar Suggu <ssuggu@AIENTERPRISE.COM>
2019-09-10 18:17:39 +0530

    Updated the config file

--- Jenkins/gulp-config.js
+++ Jenkins/gulp-config.js
@@ -1,8 +1,8 @@
 module.exports = function () {
     var instanceRoot = "C:\\inetpub\\wwwroot\\AIECorporate-cms.com";
     var config = {
-        websiteRoot: "C:\\inetpub\\wwwroot\\AIECorporate-cms.com",
-        deployTemp: "C:\AIE_Content",
+        websiteRoot: "C:\\AIE_deploy\\WebSite",
+        deployTemp: "C:\\AIE_deploy\\WebSite",
         UnicornDumpTemp: "C:\\AIE_deploy\\Unicorn",
         websiteUrl: "http://dev.aienterprise.com/",
         siteHostName: "https://10.10.23.155/",
