1ea8e0004935cd5eb79956033a0315acd76fe400
Raghu Raj T <rraj@AIENTERPRISE.COM>
2019-11-12 17:12:19 +0530

    Forms Code Gif Loader

---
+++ src/Feature/Forms/code/sitecore modules/Web/ExperienceForms/scripts/profileformgifloader.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $('.profile-loader-button').click(function () {
+        //e.preventDefault();
+        var profile_name = $('.career-field-name').val();
+        var profile_email = $('.career-field-email').val();
+        var profile_phone = $('.career-field-phone').val();
+        var profile_dropdown = $('.career-field-jobapplied').val() == 0;
+
+
+        $(".error").remove();
+
+        if (profile_name.length >= 1 && profile_email.length >= 1 && profile_phone.length >= 1 &&
+            profile_dropdown.length >=1 )
+        {
+            $('.gif-loader').show();
+            console.log("waiting");
+        }
+    });
+
+});
