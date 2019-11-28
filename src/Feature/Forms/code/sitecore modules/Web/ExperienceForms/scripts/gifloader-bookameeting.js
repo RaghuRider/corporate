a702d13a18ef22e35931d9a2fcbee5a6b5cb3aea
Raghu Raj T <rraj@AIENTERPRISE.COM>
2019-11-28 23:26:59 +0530

    form Assesment issue

--- src/Feature/Forms/code/sitecore modules/Web/ExperienceForms/scripts/gifloader-bookameeting.js
+++ src/Feature/Forms/code/sitecore modules/Web/ExperienceForms/scripts/gifloader-bookameeting.js
@@ -11,7 +11,7 @@
         if (first_name.length >= 1 && email.length >= 1 && phone.length >= 1 && org.length >= 1) {
             $('.gif-loader').show();
             $(".book-loader").css("background-color", "#3c3950");
-          //  $(".book-loader").attr("disabled", true);
+            //  $(".book-loader").attr("disabled", true);
         }
 
     });
@@ -23,4 +23,4 @@ var url = window.location.href;
 console.log(url);
 var value1 = url.substring(url.lastIndexOf('/') + 1);
 console.log(value1);
-document.getElementById("fxb_145adc60-11e2-4681-85a7-d2546a8c04aa_Fields_aa208da2-e3f9-4559-b44d-556cc40755ed__Value").value = value1; 
+document.getElementById("fxb_42670f43-c983-4139-b247-86256666930f_Fields_aa208da2-e3f9-4559-b44d-556cc40755ed__Value").value = value1; 
