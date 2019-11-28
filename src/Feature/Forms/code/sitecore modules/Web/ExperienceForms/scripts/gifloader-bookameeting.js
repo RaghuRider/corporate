b6724587461651a49a6b6545cded99a64bfc56dd
Raghu Raj T <rraj@AIENTERPRISE.COM>
2019-11-28 18:39:40 +0530

    form New changes

--- src/Feature/Forms/code/sitecore modules/Web/ExperienceForms/scripts/gifloader-bookameeting.js
+++ src/Feature/Forms/code/sitecore modules/Web/ExperienceForms/scripts/gifloader-bookameeting.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-
     $('.book-loader').click(function () {
         //e.preventDefault();
         var first_name = $('.your-name').val();
@@ -17,10 +16,11 @@
 
     });
     return false;
+
 });
 
 var url = window.location.href;
 console.log(url);
 var value1 = url.substring(url.lastIndexOf('/') + 1);
 console.log(value1);
-document.getElementsByClassName('.assesment_type').value = value1;
+document.getElementById("fxb_145adc60-11e2-4681-85a7-d2546a8c04aa_Fields_aa208da2-e3f9-4559-b44d-556cc40755ed__Value").value = value1; 
